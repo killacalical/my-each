75c4c53b005f6107568dd4d0ef4ae3cf131741b0
Rebecca <graham.cali@gmail.com>
2020-11-14 17:40:30 -0600

    commit tree 2e2ef3f2b713c421b33871d5b7e116c20c4c5396

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+
+array = [1, 2, 3, 4]
+
+def my_each(array) # put argument(s) here
+ index = 0
+ while index < array.length
+  yield(array[index])
+  index += 1
+ end
+ array
 end
+
+my_each(array) {|x| x}
